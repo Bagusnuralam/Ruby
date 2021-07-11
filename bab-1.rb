1e857c9e84289cd62e69bb5a2bc83cf4beb51998
Bagusnuralam <baguswachyu382@gmail.com>
2021-07-11 12:42:14 +0700

    initial commit

--- bab-1.rb
+++ bab-1.rb
@@ -21,6 +21,20 @@ puts 134+50
 
 puts 3**5
 
+#atau
+
+i = 3
+
+puts i += 5
+
+if i < 4
+  puts "Benar, pernyataan anda"
+elsif i > 4
+  puts "Salah, pernyataan anda"
+else
+  puts "Keduanya ngga benar"
+end
+
 
 makan = "makan yang bener" #Variabel untuk pemakaian metode
 
@@ -28,7 +42,7 @@ puts makan.upcase #Merubah menjadi kapital
 puts makan.downcase #Merubah menjadi huruf kecil
 puts makan.length #Merubah string menjadi penghitungan tiap huruf. jadi outputnya 16
 puts makan.reverse #Merubah kalimat menjadi berbalik arah
-puts makan.upcase.reverse.downcase
+puts makan.upcase.reverse.downcase #Kita bisa merubah skaligus bentuk metode
 
 
 yudi = 30
@@ -46,16 +60,18 @@ else
   puts "bukan anaknya" #Benar, dicetak apabila pernyataan pada keduanya (elsif dan if) salah maka else akan dicetak
 end 
 
-hungry = true
+hungry = false #Variabel dinyatakan benar
 
 unless hungry
-  puts "I'm writing Ruby programs!"
+  puts "I'm writing Ruby programs!" #Dicetak apabila pernyataan dari variabel bernilai salah
 else
-  puts "Time to eat!"
+  puts "Time to eat!"#Dicetak apabila pernyataan dari variabel bernilai benar 
 end
 
-olahraga = [{ali: "bentar gue cape cuy"}, {yudi: "santuy gue masih kuat"}]
+olahraga = [{ali: "bentar gue cape cuy"}, {yudi: "santuy gue masih kuat"}] #Hash didalam array
 
 olahraga.each do |lelah|#Jadi each do itu mengumpulkan data didalam variabel olahraga yang tersimpan array menjadi kumpulan satu data divariabel lelah yang saat dicetak akan langsung tersebar kumpulan data yang tersimpan divariabel baru
-  puts lelah
+  if lelah[:ali]
+    puts "Benar banget bre #{lelah[:ali]}"
+  end
 end
